18223f3aa54f344465ffc7c8a31587c7735ed777
Jakub Wrąbel <wrabel.jakub@gmail.com>
2017-04-28 20:20:40 +0200

    sdsds

--- src/main/java/pl/jwrabel/trainings/javandwro3/gui/cars/Car.java
+++ src/main/java/pl/jwrabel/trainings/javandwro3/gui/cars/Car.java
@@ -4,4 +4,58 @@ package pl.jwrabel.trainings.javandwro3.gui.cars;
  * Created by jakubwrabel on 28.04.2017.
  */
 public class Car {
+	private String brand;
+	private String model;
+	private String manufactureYear;
+	private double power;
+
+	public Car(String brand, String model, String manufactureYear, double power) {
+
+		this.brand = brand;
+		this.model = model;
+		this.manufactureYear = manufactureYear;
+		this.power = power;
+	}
+
+	@Override
+	public String toString() {
+		return "Car{" +
+				"brand='" + brand + '\'' +
+				", model='" + model + '\'' +
+				", manufactureYear='" + manufactureYear + '\'' +
+				", power=" + power +
+				'}';
+	}
+
+	public String getBrand() {
+		return brand;
+	}
+
+	public void setBrand(String brand) {
+		this.brand = brand;
+	}
+
+	public String getModel() {
+		return model;
+	}
+
+	public void setModel(String model) {
+		this.model = model;
+	}
+
+	public String getManufactureYear() {
+		return manufactureYear;
+	}
+
+	public void setManufactureYear(String manufactureYear) {
+		this.manufactureYear = manufactureYear;
+	}
+
+	public double getPower() {
+		return power;
+	}
+
+	public void setPower(double power) {
+		this.power = power;
+	}
 }
---
+++ src/main/java/pl/jwrabel/trainings/javandwro3/gui/cars/CarRepository.java
@@ -0,0 +1,24 @@
+package pl.jwrabel.trainings.javandwro3.gui.cars;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by jakubwrabel on 28.04.2017.
+ */
+public class CarRepository {
+	private List<Car> cars;
+
+	public CarRepository() {
+		cars = new ArrayList<>();
+
+		cars.add(new Car("Opel", "Vectra", "2003", 100));
+		cars.add(new Car("Ford", "Mondeo", "2010", 180));
+		cars.add(new Car("Ford", "Mustang", "2015", 280));
+		cars.add(new Car("Nissan", "Micra", "1995", 70));
+	}
+
+	public List<Car> getCars() {
+		return cars;
+	}
+}
