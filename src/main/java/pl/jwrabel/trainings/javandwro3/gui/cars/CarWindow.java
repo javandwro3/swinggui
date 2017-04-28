33809d4835e7ed9f1d47858643fef5cc6890a9dc
Jakub Wrąbel <wrabel.jakub@gmail.com>
2017-04-28 20:26:46 +0200

    sdsds

--- src/main/java/pl/jwrabel/trainings/javandwro3/gui/cars/CarWindow.java
+++ src/main/java/pl/jwrabel/trainings/javandwro3/gui/cars/CarWindow.java
@@ -1,7 +1,31 @@
 package pl.jwrabel.trainings.javandwro3.gui.cars;
 
+import javax.swing.*;
+
 /**
  * Created by jakubwrabel on 28.04.2017.
  */
-public class CarWindow {
+public class CarWindow extends JFrame {
+
+	private CarRepository carRepository;
+
+	public CarWindow() {
+		setVisible(true);
+		setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+		setSize(600, 400);
+		setLayout(null);
+
+		carRepository = new CarRepository();
+
+		String[] data = new String[]{"A", "B", "C"};
+		JList<String> jList = new JList<>(data);
+		jList.setSize(200, 300);
+		jList.setLocation(0, 0);
+		add(jList);
+
+	}
+
+	public static void main(String[] args) {
+		new CarWindow();
+	}
 }
