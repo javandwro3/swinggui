679fe4abdc3ffd14853c800578d12324bd563859
Jakub Wrąbel <wrabel.jakub@gmail.com>
2017-05-08 18:43:53 +0200

    sdsds

--- src/main/java/pl/jwrabel/trainings/javandwro3/gui/cars/CarWindow.java
+++ src/main/java/pl/jwrabel/trainings/javandwro3/gui/cars/CarWindow.java
@@ -37,18 +37,68 @@ public class CarWindow extends JFrame {
 		btnShow.setLocation(0, 350);
 		add(btnShow);
 
+		JLabel lblBrand = new JLabel("Brand");
+		lblBrand.setSize(200, 50);
+		lblBrand.setLocation(300, 0);
+		add(lblBrand);
+
+		JTextField txtBrand = new JTextField();
+		txtBrand.setSize(200, 50);
+		txtBrand.setLocation(300, 50);
+		add(txtBrand);
+
+		JLabel lblModel = new JLabel("Model");
+		lblModel.setSize(200, 50);
+		lblModel.setLocation(300, 100);
+		add(lblModel);
+
+		JTextField txtModel = new JTextField();
+		txtModel.setSize(200, 50);
+		txtModel.setLocation(300, 150);
+		add(txtModel);
+
+		JLabel lblYear = new JLabel("Manufacture year");
+		lblYear.setSize(200, 50);
+		lblYear.setLocation(300, 200);
+		add(lblYear);
+
+		JTextField txtYear = new JTextField();
+		txtYear.setSize(200, 50);
+		txtYear.setLocation(300, 250);
+		add(txtYear);
+
+		JLabel lblPower = new JLabel("Power");
+		lblPower.setSize(200, 50);
+		lblPower.setLocation(300, 300);
+		add(lblPower);
+
+		JTextField txtPower = new JTextField();
+		txtPower.setSize(200, 50);
+		txtPower.setLocation(300, 350);
+		add(txtPower);
+
 		btnShow.addActionListener(new AbstractAction() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				Car selectedCar = jList.getSelectedValue();
 				System.out.println(selectedCar);
 
-				JOptionPane.showMessageDialog(btnShow, selectedCar);
+				if (selectedCar != null) {
+					String model = selectedCar.getModel();
+					txtModel.setText(model);
+
+					txtBrand.setText(selectedCar.getBrand());
+					txtModel.setText(selectedCar.getModel());
+					txtYear.setText(selectedCar.getManufactureYear());
+
+					txtPower.setText("" + selectedCar.getPower());
+					txtPower.setText(Double.toString(selectedCar.getPower()));
+				}
+
+				// Wyświetlenie okna dialogowego
+//				JOptionPane.showMessageDialog(btnShow, selectedCar);
 			}
 		});
-
-
-
 	}
 
 	public static void main(String[] args) {
